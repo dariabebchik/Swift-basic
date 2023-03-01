dc607d2d9a6e6e2fde5260deb084582d4234620f
Dariia Bebchik <dariabebchil@gmail.com>
2023-03-01 15:29:29 +0200

    added folder "homework#2"

---
+++ homework#2/homework#2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework#2
+//
+//  Created by Дария Бебчик on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
